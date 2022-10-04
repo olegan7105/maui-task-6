bf3fca860612e0f18a0833ca79bb733f95703cfb
Пользователь <@DESKTOP-KVSP5S9>
2022-10-04 11:41:40 +0300

    1

---
+++ PointOfSale/Models/OrderType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Models
+{
+    public enum OrderType
+    {
+        [Description("Dine In")]
+        DineIn,
+        [Description("Carry Out")]
+        CarryOut,
+        [Description("Delivery")]
+        Delivery,
+
+    }
+}
