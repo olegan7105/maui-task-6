fe7d650e7ae7d877f4fcd6a0c79c076d22bdbd4a
olegan7105 <bubencrutl@mail.ru>
2022-10-11 15:35:52 +0300

    ihatemaui

---
+++ PointOfSale/Pages/Handheld/PayPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PointOfSale.Pages.Handheld;
+
+public partial class PayPage : ContentPage
+{
+	public PayPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PointOfSale/Pages/Handheld/PayViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PointOfSale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Pages.Handheld
+{
+    [INotifyPropertyChanged]
+    [QueryProperty("Order", "Order")]
+    public partial class PayViewModel
+    {
+        [ObservableProperty]
+        Order order;
+
+        [RelayCommand]
+        async void Pay()
+        {
+            var navigationParameter = new Dictionary<string, object>
+            {
+                {"Order", order }
+            };
+            await Shell.Current.GoToAsync($"{nameof(SignaturePage)}", navigationParameter)
+        }
+    }
+}
