371f3d851819d6a7e796fd9deae76594b56cad35
olegan7105 <bubencrutl@mail.ru>
2022-10-11 14:57:18 +0300

    kkk

---
+++ PointOfSale/Pages/Handheld/OrdersViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PointOfSale.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Pages.Handheld
+{
+    [INotifyPropertyChanged]
+    public partial class OrdersViewModel
+    {
+        [ObservableProperty]
+        private ObservableCollection<Order> _orders;
+        public OrdersViewModel()
+        {
+            _orders = new ObservableCollection<Order>(AppData.Orders);
+        }
+    }
+}
