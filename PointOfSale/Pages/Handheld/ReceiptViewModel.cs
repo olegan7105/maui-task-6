13905f788a98f05a6ce0a63b855740cd08b75e9e
olegan7105 <bubencrutl@mail.ru>
2022-11-15 11:46:08 +0300

    k

---
+++ PointOfSale/Pages/Handheld/ReceiptViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PointOfSale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Pages.Handheld
+{
+    [INotifyPropertyChanged]
+    [QueryProperty("Order", "Order")]
+    public partial class ReceiptViewModel
+    {
+        [ObservableProperty]
+        Order order;
+
+        [RelayCommand]
+        async void Done()
+        {
+            await Shell.Current.GoToAsync("///orders");
+        }
+    }
+}
---
+++ PointOfSale/Pages/Handheld/ScanPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PointOfSale.Pages.Handheld;
+
+public partial class ScanPage : ContentPage
+{
+	public ScanPage()
+	{
+		InitializeComponent();
+	}
+}
